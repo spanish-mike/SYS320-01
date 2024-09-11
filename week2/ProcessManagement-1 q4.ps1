5c76e9bf855400e552252a266c362cae4c62d7e6
Miguel <mig.90302@gmail.com>
2024-09-10 23:45:04 -0400

    questions 1, 2 and 3 should work, question 4 does not yet

---
+++ week2/ProcessManagement-1 q4.ps1
@@ -0,0 +1,4 @@
+$array = Get-Process | SELECT ProcessName
+if ($array -Contains "*chrome*")
+{Stop-Process -Name chrome}
+else {Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "Champlain.edu"}

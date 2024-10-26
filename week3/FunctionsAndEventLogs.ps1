1e1f91310c98201b54b1b0b571737b2170fe6ca4
Miguel <mig.90302@gmail.com>
2024-10-25 21:06:06 -0400

    Functions and event logs is done

--- week3/FunctionsAndEventLogs.ps1
+++ week3/FunctionsAndEventLogs.ps1
@@ -1,26 +1,57 @@
-#$logArray = @()
-$loginouts = Get-EventLog -Logname System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
 
-$loginoutsTable = @() #empty array to fill with logs
+function getEventLogs($amountOfDaysCovered){
+    $loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$amountOfDaysCovered)
 
-for($i=0; $i -lt  $loginouts.Count; $i++){
+    $loginoutsTable = @() #empty array to fill with logs
 
-# Creating event property value
-$event = ""
-if($loginouts[$i].InstanceId -eq 7001) {$event="Logon"}
-if($loginouts[$i].InstanceId -eq 7002) {$event="Logoff"}
+    for($i=0; $i -lt  $loginouts.Count; $i++){
 
-# Creating user property value
-$userSID = $loginouts[$i].ReplacementStrings[1]
+        # Creating event property value
+        $event = ""
+        if($loginouts[$i].InstanceId -eq 7001) {$event="Logon"}
+        if($loginouts[$i].InstanceId -eq 7002) {$event="Logoff"}
 
-$user = (New-Object System.Security.Principal.SecurityIdentifier($userSID)).Translate([System.Security.Principal.NTAccount])
+        # Creating user property value
+        $userSID = $loginouts[$i].ReplacementStrings[1]
 
-#adding each new line (in the form of custom object)
-$loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeWritten; `
-                                       "Id" = $loginouts[$i].EventID; `
-                                    "Event" = $event; `
-                                     "User" = $user;
-                                    }
-# end of for loop :)
+        $user = (New-Object System.Security.Principal.SecurityIdentifier($userSID)).Translate([System.Security.Principal.NTAccount])
+
+        #adding each new line (in the form of custom object)
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeWritten; `
+                                           "Id" = $loginouts[$i].EventID; `
+                                        "Event" = $event; `
+                                         "User" = $user;
+                                        }
+    # end of for loop :)
+    }
+    return $loginoutsTable
 }
-$loginoutsTable
+
+#FunctionsAndEventLogsTheFunction(14)
+
+function getStartAndShutDownLogs($amountOfDaysCovered) {
+    $loginouts = Get-EventLog System -Source EventLog -After (Get-Date).AddDays(-$amountOfDaysCovered)
+
+    $loginoutsTable = @() #empty array to fill with logs
+
+    for($i=0; $i -lt  $loginouts.Count; $i++){
+
+        # Creating event property value
+        $event = ""
+        if($loginouts[$i].EventID -eq 6005) {$event="Shutdown"}
+        elseif($loginouts[$i].EventID -eq 6006) {$event="Startup"}
+        else {continue}                                            #any event that isnt shutdown or startup is ignored
+
+        # Creating user property value
+        $userSID = $loginouts[$i].ReplacementStrings[1]
+
+        #adding each new line (in the form of custom object)
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeWritten; `
+                                               "Id" = $loginouts[$i].EventID; `
+                                            "Event" = $event; `
+                                             "User" = "System";
+                                        }
+    } # end of for loop
+    return $loginoutsTable
+}
+
--- week3/main.ps1
+++ week3/main.ps1
@@ -1,5 +1,9 @@
-. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot FunctionsAndEventLogs.ps1)
 
-$outputVariable = Find-All-Visitors "index.html" "200" "Chrome"
 
-$outputVariable | Select-Object Count, IP, Name
+$loginoutsTable = getEventLogs  15
+$loginoutsTable
+
+$loginoutsTable = getStartAndShutDownLogs 25
+$loginoutsTable
+
